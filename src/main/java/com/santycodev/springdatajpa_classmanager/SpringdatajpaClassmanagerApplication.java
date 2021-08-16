a8fc5a3d225f394a317e2d915c4ca52665a286e8
Santy <santi.codev@gmail.com>
2021-08-16 18:19:12 +0200

    DEV: Creacion de proyecto

---
+++ src/main/java/com/santycodev/springdatajpa_classmanager/SpringdatajpaClassmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.santycodev.springdatajpa_classmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaClassmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaClassmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santycodev/springdatajpa_classmanager/SpringdatajpaClassmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santycodev.springdatajpa_classmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaClassmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
